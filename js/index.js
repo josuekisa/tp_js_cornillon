b006bc052b3341b106e29ff149dc18524781239f
josuekisa <josue.kisa31@gmail.com>
2023-01-13 16:21:23 +0100

    debut js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+/*var adminLink = document.getElementById("admin-link");
+adminLink.addEventListener("click", function (event) {
+  event.preventDefault(); // empêche le navigateur de charger une nouvelle page
+  window.open("http://127.0.0.1:5501/index.html#admin", "https://google.com"); // ouvre un nouvel onglet avec l'URL spécifiée
+});
+*/
+
---
+++ js/tab.js
@@ -0,0 +1,12 @@
+
+const nav= document.querySelector('nav');
+export class Tab{
+constructor(){
+    document
+         .querySelector('nav')
+         .innerHTML=`<div class="tab">Accueil</div>`;
+       
+}
+}
+
+new Tab();
